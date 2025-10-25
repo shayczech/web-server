5552b8da240d8c0eb1553225138a218b23a9a947
Shaylee Czech <shayleeczech@Shaylees-MacBook-Pro.local>
2025-10-24 21:03:22 -0500

    fix: Final Key Pair fix - switched to new key pair 'new-web-key' to resolve caching/InvalidKeyPair error.

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ module "web_server" {
   # --- Provide values for the module's input variables ---
   ami_id          = var.ami_id                        # Pass the value from the root variables.tf
   instance_type   = var.instance_type                 # Pass the value from the root variables.tf
-  key_name        = "web-key"  # Updated to use the data source
+  key_name        = "new-web-key"  # Updated to use the data source
   index_html_path = "${path.root}/index.html"         # Provide the path to the index.html file in the root directory
   server_name     = "web-server"                      # Optionally override the default name (defined in module variables.tf)
 }
